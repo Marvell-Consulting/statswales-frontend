8a43f01806293e0d15f4a6a178a6d752ea84c570
Phil Moorhouse <phil@wheelsandcogs.co.uk>
2024-09-27 14:32:30 +0100

    use new eslint conf

---
+++ eslint.config.mjs
@@ -0,0 +1,33 @@
+// @ts-check
+import shopifyEslintPlugin from '@shopify/eslint-plugin';
+
+export default [
+  {
+    // global ignores need to be in their own block otherwise they don't seem to work
+    ignores: [
+      '.github/**',
+      '.vscode/**',
+      'coverage/**',
+      'dist/**',
+      'node_modules/**',
+      '**/*.config.{mjs,ts}'
+    ],
+  },
+  ...shopifyEslintPlugin.configs.typescript,
+  ...shopifyEslintPlugin.configs.prettier,
+  {
+    rules: {
+      'line-comment-position': 'off',
+      'no-warning-comments': 'warn',
+      'no-process-env': 'warn',
+      '@typescript-eslint/member-ordering': 'off',
+      '@typescript-eslint/naming-convention': [
+        'error',
+        {
+          selector: 'default',
+          format: ['camelCase', 'PascalCase', 'UPPER_CASE', 'snake_case'],
+        }
+      ],
+    }
+  },
+];
