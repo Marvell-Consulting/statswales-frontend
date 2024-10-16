ddaba28f4c6b4b792198772befd1fe6b5fce2c0d
Phil Moorhouse <phil@wheelsandcogs.co.uk>
2024-10-16 14:40:09 +0100

    don't bootstrap services for static urls, use cookie for jwt

--- src/middleware/services.ts
+++ src/middleware/services.ts
@@ -8,8 +8,10 @@ import { localeUrl } from './language-switcher';
 // initialise any request-scoped services required by the app and store them on the request object for later use
 // see interfaces/service-container.ts and @types/express/index.d.ts for details
 export const initServices = (req: Request, res: Response, next: NextFunction): void => {
-    req.swapi = new StatsWalesApi(req.language as Locale, req.jwt);
-    req.buildUrl = localeUrl; // for controllers
-    res.locals.buildUrl = localeUrl; // for templates
+    if (!/^\/(public|css|assets)/.test(req.originalUrl)) {
+        req.swapi = new StatsWalesApi(req.language as Locale, req.cookies.jwt);
+        req.buildUrl = localeUrl; // for controllers
+        res.locals.buildUrl = localeUrl; // for templates
+    }
     next();
 };
