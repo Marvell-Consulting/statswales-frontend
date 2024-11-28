2fedf3461402a98372a81e42e7641558d904313e
Jamie Maynard <29251905+j-maynard@users.noreply.github.com>
2024-11-28 17:07:41 +0000

    Fix a few bugs in pagination and table views
    Fixes a few bugs in the pagination code.  Moves reusable code out to
    utilities as per agreement in previous PR.  Fixes some minor issues with
    preview and data views.
    
    The developer view now introduces a possible table component allowing
    for overflow on x and y for big tables and introduces sticky table
    headers to make viewing the tables easier.  Should be tested with users
    and user research later.

--- src/utils/pagination.ts
+++ src/utils/pagination.ts
@@ -1,30 +1,62 @@
+/* eslint-disable @typescript-eslint/naming-convention */
+
 // Special thanks ChatGPT...  The GovUK pagination algorithm
 export function generateSequenceForNumber(highlight: number, end: number): (string | number)[] {
     const sequence: (string | number)[] = [];
 
     // Validate input
-    if (highlight > end) {
+    if (highlight < 1 || highlight > end) {
         throw new Error(`Highlighted number must be between 1 and ${end}.`);
     }
 
-    // Numbers before the highlighted number
-    if (highlight - 1 > 1) {
+    if (end - 1 < 3) {
+        sequence.push(
+            ...Array.from({ length: end - 1 + 1 }, (_, index) => 1 + index).map((num) =>
+                num === highlight ? num : num
+            )
+        );
+        return sequence;
+    }
+
+    // Case 1: Highlight is within the first 3 pages
+    if (highlight <= 3) {
+        sequence.push(...Array.from({ length: 3 }, (_, index) => 1 + index));
+        sequence[highlight - 1] = highlight; // Highlight the specific number
+        if (end > 4) {
+            sequence.push('...');
+            sequence.push(end);
+        }
+        return sequence;
+    }
+
+    // Case 2: Highlight is near or at the last 3 pages
+    if (highlight >= end - 2) {
+        if (end - 3 > 1) {
+            sequence.push(1, '...');
+        }
+        for (let i = end - 3; i <= end; i++) {
+            if (i === highlight) {
+                sequence.push(i);
+            } else {
+                sequence.push(i);
+            }
+        }
+        return sequence;
+    }
+
+    // Case 3: General case
+    if (highlight - 2 > 1) {
         sequence.push(1, '...');
         sequence.push(highlight - 1);
     } else {
-        // eslint-disable-next-line @typescript-eslint/naming-convention
         sequence.push(...Array.from({ length: highlight - 1 }, (_, index) => index + 1));
     }
 
-    // Highlighted number
-    sequence.push(highlight);
+    sequence.push(highlight); // Highlight the number
 
-    // Numbers after the highlighted number
     if (highlight + 1 < end) {
-        sequence.push(highlight + 1);
-        sequence.push('...', end);
+        sequence.push(highlight + 1, '...', end);
     } else {
-        // eslint-disable-next-line @typescript-eslint/naming-convention
         sequence.push(...Array.from({ length: end - highlight }, (_, index) => highlight + 1 + index));
     }
 
