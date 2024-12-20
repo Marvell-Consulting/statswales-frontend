8cd8cf71073ec2db68fb30b7935f675d392dcb3d
Phil Moorhouse <phil@wheelsandcogs.co.uk>
2024-12-20 16:49:07 +0000

    test setup cleanup

--- tests/fixtures/datasets.ts
+++ tests/fixtures/datasets.ts
@@ -1,12 +1,19 @@
-import { DeepPartial } from '../../src/@types/deep-partial';
 import { DatasetDTO } from '../../src/dtos/dataset';
 
-export const dataset1: DeepPartial<DatasetDTO> = {
+import { publisher1 } from './users';
+
+export const dataset1: DatasetDTO = {
   id: '936c1ab4-2b33-4b13-8949-4316a156d24b',
+  created_by: publisher1.id,
+  created_at: new Date().toISOString(),
+  revisions: [],
   datasetInfo: [{ language: 'en-GB', title: 'Upload Test' }]
 };
 
-export const dataset2: DeepPartial<DatasetDTO> = {
+export const dataset2: DatasetDTO = {
   id: 'fb440a0d-a4fb-40cb-b9e2-3f88659a5343',
-  datasetInfo: [{ language: 'en-GB', title: 'Sources Test' }]
+  created_by: publisher1.id,
+  created_at: new Date().toISOString(),
+  revisions: [],
+  datasetInfo: [{ language: 'en-GB', title: 'Preview Test' }]
 };
--- tests/setup/auth.setup.ts
+++ tests/setup/auth.setup.ts
@@ -4,27 +4,10 @@ import JWT from 'jsonwebtoken';
 import { appConfig } from '../../src/config';
 import { User } from '../../src/interfaces/user.interface';
 import { adminContext, publisherContext, approverContext } from '../../playwright/.auth/contexts';
+import { admin1, publisher1, approver1 } from '../fixtures/users';
 
 const config = appConfig();
 const jwtSecret = config.auth.jwt.secret;
-
-export const adminUser: User = {
-  id: '044d94c5-91ba-495e-a718-31c597a0a30b',
-  name: 'Tom Admin',
-  email: 'tom.admin@example.com'
-};
-
-export const publisherUser: User = {
-  id: 'f3dc1ae6-273e-4ac9-a498-ba2813c51c24',
-  name: 'Joe Publisher',
-  email: 'joe.publisher@example.com'
-};
-
-export const approverUser: User = {
-  id: 'ce08727e-dd3f-48cc-921a-cae5c4dd4a18',
-  name: 'Frank Approver',
-  email: 'frank.approver@example.com'
-};
 
 const jwtCookie = (user: User) => {
   return {
@@ -36,16 +19,16 @@ const jwtCookie = (user: User) => {
 };
 
 setup('authenticate as admin', async ({ page }) => {
-  await page.context().addCookies([jwtCookie(adminUser)]);
+  await page.context().addCookies([jwtCookie(admin1)]);
   await page.context().storageState({ path: adminContext });
 });
 
 setup('authenticate as publisher', async ({ page }) => {
-  await page.context().addCookies([jwtCookie(publisherUser)]);
+  await page.context().addCookies([jwtCookie(publisher1)]);
   await page.context().storageState({ path: publisherContext });
 });
 
 setup('authenticate as approver', async ({ page }) => {
-  await page.context().addCookies([jwtCookie(approverUser)]);
+  await page.context().addCookies([jwtCookie(approver1)]);
   await page.context().storageState({ path: approverContext });
 });
