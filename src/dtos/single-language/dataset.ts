843dd0bbec43323b37dd4675e40e923d9f1f9f84
Phil Moorhouse <phil@wheelsandcogs.co.uk>
2025-03-13 08:46:14 +0000

    fix prettier cli

--- src/dtos/single-language/dataset.ts
+++ src/dtos/single-language/dataset.ts
@@ -5,19 +5,19 @@ import { SingleLanguageRevision } from './revision';
 import { SingleLanguageMeasure } from './measure';
 
 export interface SingleLanguageDataset {
-    id: string;
-    created_at: string;
-    created_by: string;
-    live?: string;
-    archive?: string;
-    measure?: SingleLanguageMeasure;
-    dimensions?: SingleLanguageDimension[];
-    revisions?: SingleLanguageRevision[];
-    start_revision?: SingleLanguageRevision;
-    end_revision?: SingleLanguageRevision;
-    draft_revision?: SingleLanguageRevision;
-    published_revision?: SingleLanguageRevision;
-    team?: TeamDTO;
-    start_date?: string;
-    end_date?: string;
+  id: string;
+  created_at: string;
+  created_by: string;
+  live?: string;
+  archive?: string;
+  measure?: SingleLanguageMeasure;
+  dimensions?: SingleLanguageDimension[];
+  revisions?: SingleLanguageRevision[];
+  start_revision?: SingleLanguageRevision;
+  end_revision?: SingleLanguageRevision;
+  draft_revision?: SingleLanguageRevision;
+  published_revision?: SingleLanguageRevision;
+  team?: TeamDTO;
+  start_date?: string;
+  end_date?: string;
 }
