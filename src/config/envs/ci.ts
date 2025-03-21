adf8d7529168d2525dbf39ea3151a53218b38516
Phil Moorhouse <phil@wheelsandcogs.co.uk>
2025-03-21 15:22:15 +0000

    switch ci logging back to silent

--- src/config/envs/ci.ts
+++ src/config/envs/ci.ts
@@ -11,7 +11,7 @@ export function getCIConfig(): AppConfig {
     env: AppEnv.Ci,
     supportEmail: 'support@example.com',
     logger: {
-      level: 'debug'
+      level: 'silent'
     },
     frontend: {
       port: 3000,
