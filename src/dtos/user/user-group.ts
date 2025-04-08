c3b0792ca6dc5e0af01caedfad66b9d8c0792e1f
Phil Moorhouse <phil@wheelsandcogs.co.uk>
2025-04-08 16:39:06 +0100

    view users in a group

--- src/dtos/user/user-group.ts
+++ src/dtos/user/user-group.ts
@@ -1,15 +1,15 @@
 import { DatasetDTO } from '../dataset';
 import { OrganisationDTO } from '../organisation';
 
-import { UserDTO } from './user';
 import { UserGroupMetadataDTO } from './user-group-metadata-dto';
+import { UserWithRolesDTO } from './user-with-roles-dto';
 
 export interface UserGroupDTO {
   id: string;
   prefix?: string;
   organisation_id?: string;
   organisation?: OrganisationDTO;
-  users?: UserDTO[];
+  users?: UserWithRolesDTO[];
   user_count?: number;
   datasets?: DatasetDTO[];
   dataset_count?: number;
