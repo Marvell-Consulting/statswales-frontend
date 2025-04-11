e128ad3a3a4cccc424d65ae5c0fca685a76fd4e1
Phil Moorhouse <phil@wheelsandcogs.co.uk>
2025-04-11 17:47:28 +0100

    use the helper

--- src/routes/homepage.ts
+++ src/routes/homepage.ts
@@ -4,6 +4,7 @@ import { flashErrors, flashMessages } from '../middleware/flash';
 import { DatasetListItemDTO } from '../dtos/dataset-list-item';
 import { ResultsetWithCount } from '../interfaces/resultset-with-count';
 import { getPaginationProps } from '../utils/pagination';
+import { getEditorUserGroups } from '../utils/get-editor-user-groups';
 
 export const homepage = Router();
 
@@ -21,8 +22,8 @@ homepage.get('/', async (req: Request, res: Response, next: NextFunction) => {
     const page = parseInt(req.query.page_number as string, 10) || 1;
     const limit = parseInt(req.query.page_size as string, 10) || 20;
 
-    // user must be in at least one group to start a new dataset
-    const canCreate = req.user?.groups?.length || 0 > 0;
+    // user must be an editor in at least one group to start a new dataset
+    const canCreate = getEditorUserGroups(req.user).length > 0;
 
     const results: ResultsetWithCount<DatasetListItemDTO> = await req.pubapi.getDatasetList(page, limit);
     const { data, count } = results;
