e4bc9ba7c5370470960a53b0e125507211ecbb4a
Phil Moorhouse <phil@wheelsandcogs.co.uk>
2025-05-27 16:03:45 +0100

    fix linting

--- src/views/components/consumer/Hero.jsx
+++ src/views/components/consumer/Hero.jsx
@@ -1,9 +1,5 @@
 import React from 'react';
 
 export default function Hero({ children }) {
-  return (
-    <div className="hero">
-      {children}
-    </div>
-  );
+  return <div className="hero">{children}</div>;
 }
--- src/views/consumer/topic-list.jsx
+++ src/views/consumer/topic-list.jsx
@@ -13,15 +13,23 @@ function Breadcrumbs({ parentTopics, selectedTopic }) {
       <nav className="govuk-breadcrumbs" aria-label="Breadcrumb">
         <ol className="govuk-breadcrumbs__list">
           <li className="govuk-breadcrumbs__list-item">
-            <a className="govuk-breadcrumbs__link" href={buildUrl(`/published`, i18n.language)}>Home</a>
+            <a className="govuk-breadcrumbs__link" href={buildUrl(`/published`, i18n.language)}>
+              Home
+            </a>
           </li>
 
           {parentTopics.map((topic, index) => (
             <li className="govuk-breadcrumbs__list-item" key={topic.id}>
-              { topic.id === selectedTopic.id
-                ? topic.name
-                : <a className="govuk-breadcrumbs__link" href={buildUrl(`/published/topic/${topic.id}/${topic.slug}`, i18n.language)}>{topic.name}</a>
-              }
+              {topic.id === selectedTopic.id ? (
+                topic.name
+              ) : (
+                <a
+                  className="govuk-breadcrumbs__link"
+                  href={buildUrl(`/published/topic/${topic.id}/${topic.slug}`, i18n.language)}
+                >
+                  {topic.name}
+                </a>
+              )}
             </li>
           ))}
         </ol>
@@ -35,7 +43,7 @@ function ChildTopics({ childTopics }) {
 
   return (
     <ul className="govuk-list">
-      { childTopics.map((topic) => (
+      {childTopics.map((topic) => (
         <li className="index-list__item" key={topic.id}>
           <a href={buildUrl(`/published/topic/${topic.id}/${topic.slug}`, i18n.language)}>
             <h3 className="govuk-heading-s govuk-!-margin-bottom-0">{topic.name}</h3>
@@ -51,7 +59,7 @@ function Datasets({ datasets }) {
 
   return (
     <ul className="govuk-list">
-      { datasets.map((dataset) => (
+      {datasets.map((dataset) => (
         <li className="index-list__item" key={dataset.id}>
           <a href={buildUrl(`/published/${dataset.id}`, i18n.language)}>
             <h3 className="govuk-heading-s govuk-!-margin-bottom-0">{dataset.title}</h3>
@@ -59,7 +67,9 @@ function Datasets({ datasets }) {
           <div className="index-list__meta">
             <p className="govuk-!-margin-top-0">
               <span className="govuk-body-s caption index-list__item__meta">
-                <T published={dateFormat(dataset.published_date, 'd MMMM yyyy')}>consumer.topic_list.dataset.first_published</T>
+                <T published={dateFormat(dataset.published_date, 'd MMMM yyyy')}>
+                  consumer.topic_list.dataset.first_published
+                </T>
               </span>
             </p>
           </div>
@@ -72,7 +82,7 @@ function Datasets({ datasets }) {
 export default function TopicList(props) {
   return (
     <Layout {...props}>
-      { !props.selectedTopic &&
+      {!props.selectedTopic && (
         <Hero>
           <div className="govuk-width-container">
             <div className="govuk-grid-row govuk-!-margin-bottom-6">
@@ -84,29 +94,29 @@ export default function TopicList(props) {
             </div>
           </div>
         </Hero>
-      }
+      )}
 
       <div className="govuk-grid-row">
         <div className="govuk-grid-column-two-thirds">
-
-          { !props.selectedTopic &&
+          {!props.selectedTopic && (
             <h2 className="govuk-heading-l">
               <T>consumer.topic_list.topics</T>
             </h2>
-          }
+          )}
 
-          { props.selectedTopic &&
+          {props.selectedTopic && (
             <>
               <Breadcrumbs {...props} />
-              <h2 className="topic-subhead"><T>consumer.topic_list.topic</T></h2>
+              <h2 className="topic-subhead">
+                <T>consumer.topic_list.topic</T>
+              </h2>
               <h1 className="govuk-heading-xl">{props.selectedTopic.name}</h1>
             </>
-          }
+          )}
 
-          { props.childTopics?.length > 0 && <ChildTopics {...props} /> }
+          {props.childTopics?.length > 0 && <ChildTopics {...props} />}
 
-          { props.datasets?.length > 0 && <Datasets {...props} /> }
-
+          {props.datasets?.length > 0 && <Datasets {...props} />}
         </div>
       </div>
     </Layout>
