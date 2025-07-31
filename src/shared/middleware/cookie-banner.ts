9af8433289f54cc5d511f847c6bae4db1f74c13a
Phil Moorhouse <phil@wheelsandcogs.co.uk>
2025-07-31 11:30:22 +0100

    fix saved notification

--- src/shared/middleware/cookie-banner.ts
+++ src/shared/middleware/cookie-banner.ts
@@ -4,7 +4,7 @@ import { CookiePreferences } from '../interfaces/cookie-preferences';
 
 export const cookieBanner = (req: Request, res: Response, next: NextFunction) => {
   const cookiePreferences = req.cookies['cookiePref'] as CookiePreferences;
-  res.locals.showCookieBanner = !cookiePreferences;
+  res.locals.showCookieBanner = !cookiePreferences || cookiePreferences.showBanner;
 
   if (res.locals.showCookieBanner) {
     logger.debug('new visitor or user previously refused cookies - displaying cookie banner');
--- src/shared/views/components/CookieSettings.tsx
+++ src/shared/views/components/CookieSettings.tsx
@@ -8,6 +8,8 @@ type CookieSettingsProps = {
   content: string;
   tableOfContents: string;
   cookiePreferences: CookiePreferences;
+  saved?: boolean;
+  referrer?: string;
 };
 
 export default function CookieSettings(props: CookieSettingsProps) {
@@ -16,6 +18,35 @@ export default function CookieSettings(props: CookieSettingsProps) {
 
   return (
     <>
+      {showBanner && (
+        <div className="govuk-notification-banner" data-module="govuk-notification-banner">
+          <div className="govuk-notification-banner__content">
+            <h3 className="govuk-notification-banner__heading govuk-heading-m">
+              <T>cookies.settings.not_saved.heading</T>
+            </h3>
+            <p className="govuk-body">
+              <T>cookies.settings.not_saved.content</T>
+            </p>
+          </div>
+        </div>
+      )}
+
+      {props.saved && (
+        <div
+          className="govuk-notification-banner govuk-notification-banner--success"
+          data-module="govuk-notification-banner"
+        >
+          <div className="govuk-notification-banner__content">
+            <h3 className="govuk-notification-banner__heading govuk-heading-m">
+              <T>cookies.settings.saved.heading</T>
+            </h3>
+            <a href={props.referrer} className="govuk-link">
+              <T>cookies.settings.saved.link</T>
+            </a>
+          </div>
+        </div>
+      )}
+
       <div className="govuk-grid-row">
         <div className="govuk-grid-column-two-thirds" dangerouslySetInnerHTML={{ __html: props.content }} />
         <div style={{ position: 'sticky', top: '10px' }} className="govuk-grid-column-one-third">
@@ -23,6 +54,7 @@ export default function CookieSettings(props: CookieSettingsProps) {
           <div dangerouslySetInnerHTML={{ __html: props.tableOfContents }}></div>
         </div>
       </div>
+
       <div className="govuk-grid-row">
         <div className="govuk-grid-column-two-thirds">
           <form method="POST" action={buildUrl('/cookies', i18n.language)} className="govuk-form-group">
