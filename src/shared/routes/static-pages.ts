06e482abc52c7ebbb093488e6a8a34a8558f2110
Phil Moorhouse <phil@wheelsandcogs.co.uk>
2025-08-14 16:55:42 +0100

    add docs for shorthand note codes

--- src/shared/routes/static-pages.ts
+++ src/shared/routes/static-pages.ts
@@ -16,28 +16,33 @@ const docsPath = path.join(__dirname, '..', '..', '..', 'docs', 'static-pages');
 
 export const staticPages = Router();
 
-staticPages.get('/accessibility', async (req: Request, res: Response, next: NextFunction) => {
-  const lang = req.language.split('-')[0]?.toLowerCase() || 'en';
-  const requestedFilePath = path.join(docsPath, `accessibility.${lang}.md`);
-  const normalizedFilePath = path.resolve(requestedFilePath);
+const staticPageHandler = (pageName: string) => {
+  return async (req: Request, res: Response, next: NextFunction) => {
+    const lang = req.language.split('-')[0]?.toLowerCase() || 'en';
+    const requestedFilePath = path.join(docsPath, `${pageName}.${lang}.md`);
+    const normalizedFilePath = path.resolve(requestedFilePath);
 
-  if (!normalizedFilePath.startsWith(docsPath) || !fs.existsSync(normalizedFilePath)) {
-    logger.error('Could not find accessibility docs file');
-    next(new NotFoundException());
-    return;
-  }
+    if (!normalizedFilePath.startsWith(docsPath) || !fs.existsSync(normalizedFilePath)) {
+      logger.error(`Could not load ${normalizedFilePath}`);
+      next(new NotFoundException());
+      return;
+    }
 
-  try {
-    const title = t('accessibility.title', { lng: req.language });
-    const markdownFile: string = await readFile(normalizedFilePath, 'utf8');
-    const { window } = new JSDOM(`<!DOCTYPE html>`);
-    const domPurify = DOMPurify(window);
-    const tableOfContents = createToc(markdownFile);
-    marked.use({ renderer: docRenderer });
-    const content = domPurify.sanitize(await marked.parse(markdownFile));
-    res.render('static-page', { content, tableOfContents, title });
-  } catch (err) {
-    logger.error(err, 'Could not render accessibility page');
-    next(new NotFoundException());
-  }
-});
+    try {
+      const title = t(`${pageName}.title`, { lng: req.language });
+      const markdownFile: string = await readFile(normalizedFilePath, 'utf8');
+      const { window } = new JSDOM(`<!DOCTYPE html>`);
+      const domPurify = DOMPurify(window);
+      const tableOfContents = createToc(markdownFile);
+      marked.use({ renderer: docRenderer });
+      const content = domPurify.sanitize(await marked.parse(markdownFile));
+      res.render('static-page', { content, tableOfContents, title });
+    } catch (err) {
+      logger.error(err, `Could not render ${pageName} page`);
+      next(new NotFoundException());
+    }
+  };
+};
+
+staticPages.get('/accessibility', staticPageHandler('accessibility'));
+staticPages.get('/shorthand', staticPageHandler('shorthand'));
