256772d9ada529915191e736ff288530106d947b
Phil Moorhouse <phil@wheelsandcogs.co.uk>
2025-09-09 17:16:41 +0100

    show correct actions

--- src/publisher/views/publish/overview/ActionsTab.tsx
+++ src/publisher/views/publish/overview/ActionsTab.tsx
@@ -6,6 +6,8 @@ import { useLocals } from '../../../../shared/views/context/Locals';
 import { appConfig } from '../../../../shared/config';
 import { TaskAction } from '../../../../shared/enums/task-action';
 import { TaskStatus } from '../../../../shared/enums/task-status';
+import { DatasetStatus } from '../../../../shared/enums/dataset-status';
+import { PublishingStatus } from '../../../../shared/enums/publishing-status';
 
 const config = appConfig();
 
@@ -37,8 +39,8 @@ type ActionsTabProps = {
   canEdit: boolean;
   canApprove: boolean;
   canMoveGroup: boolean;
-  datasetStatus: string;
-  publishingStatus: string;
+  datasetStatus: DatasetStatus;
+  publishingStatus: PublishingStatus;
   openTasks: TaskDTO[];
 };
 
@@ -126,7 +128,7 @@ export function ActionsTab({
       </div>
 
       <ul className="govuk-list">
-        {canEdit && publishingStatus === 'incomplete' && (
+        {canEdit && publishingStatus === PublishingStatus.Incomplete && (
           <li>
             <ActionLink path={`/publish/${datasetId}/tasklist`} action="continue" />
           </li>
@@ -138,7 +140,7 @@ export function ActionsTab({
           </li>
         )}
 
-        {datasetStatus === 'live' && (
+        {[DatasetStatus.Live, DatasetStatus.Archived].includes(datasetStatus) && (
           <li>
             <ActionLink
               fullUrl={`${consumerUrl}/${i18n.language}/${datasetId}`}
@@ -148,24 +150,41 @@ export function ActionsTab({
           </li>
         )}
 
-        {canEdit && publishingStatus === 'published' && openTasks.length === 0 && (
-          <li>
-            <ActionLink path={`/publish/${datasetId}/update`} action="update_dataset" />
-          </li>
-        )}
-
-        {(canEdit || canApprove) && publishingStatus === 'published' && openTasks.length === 0 && (
-          <>
+        {canEdit &&
+          datasetStatus === DatasetStatus.Live &&
+          publishingStatus === PublishingStatus.Published &&
+          openTasks.length === 0 && (
             <li>
-              <ActionLink path={`/publish/${datasetId}/unpublish`} action="unpublish_dataset" />
+              <ActionLink path={`/publish/${datasetId}/update`} action="update_dataset" />
             </li>
-            <li>
-              <ActionLink path={`/publish/${datasetId}/archive`} action="archive_dataset" />
-            </li>
-          </>
-        )}
+          )}
 
-        {publishingStatus !== 'published' && (
+        {(canEdit || canApprove) &&
+          datasetStatus === DatasetStatus.Live &&
+          publishingStatus === PublishingStatus.Published &&
+          openTasks.length === 0 && (
+            <>
+              <li>
+                <ActionLink path={`/publish/${datasetId}/unpublish`} action="unpublish_dataset" />
+              </li>
+              <li>
+                <ActionLink path={`/publish/${datasetId}/archive`} action="archive_dataset" />
+              </li>
+            </>
+          )}
+
+        {(canEdit || canApprove) &&
+          datasetStatus === DatasetStatus.Archived &&
+          publishingStatus === PublishingStatus.Published &&
+          openTasks.length === 0 && (
+            <>
+              <li>
+                <ActionLink path={`/publish/${datasetId}/unarchive`} action="unarchive_dataset" />
+              </li>
+            </>
+          )}
+
+        {publishingStatus !== PublishingStatus.Published && (
           <li>
             <ActionLink path={`/publish/${datasetId}/cube-preview`} action="preview" newTab />
           </li>
