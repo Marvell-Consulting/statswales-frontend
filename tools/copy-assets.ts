20202aec4a3888ceec45f76c66eed29e467b69f5
Ben Marvell <ben@marvell.consulting>
2025-10-01 12:28:23 +0100

    Serve up a robots.txt

--- tools/copy-assets.ts
+++ tools/copy-assets.ts
@@ -4,4 +4,5 @@ shell.cp('-R', 'src/consumer/views', 'dist/consumer/');
 shell.cp('-R', 'src/shared/views', 'dist/shared/');
 shell.cp('-R', 'src/shared/public/assets', 'dist/public/');
 shell.cp('-R', 'src/shared/public/css', 'dist/public/');
+shell.cp('src/shared/public/robots.txt', 'dist/public/');
 shell.cp('-R', 'src/shared/i18n', 'dist/shared/');
