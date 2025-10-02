bbdd00fae7482ccce1dba63b374c373debfa976f
Ben Marvell <ben@marvell.consulting>
2025-10-02 13:33:44 +0100

    Remove FRONTEND_URL as its replaced with CONSUMER_URL

--- tests/.jest/set-env-vars.ts
+++ tests/.jest/set-env-vars.ts
@@ -1,4 +1,4 @@
-// process.env.FRONTEND_URL = 'http://example.com:3000';
+// process.env.CONSUMER_URL = 'http://example.com:3000';
 // process.env.BACKEND_URL = 'http://example.com:3001';
 // process.env.SESSION_SECRET = 'mysecret';
 // process.env.JWT_SECRET = 'mysecret';
