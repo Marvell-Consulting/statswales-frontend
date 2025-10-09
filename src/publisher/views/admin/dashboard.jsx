6473866669c8b6646dec26e7efc14484ace9ab82
Phil Moorhouse <phil@wheelsandcogs.co.uk>
2025-10-09 19:59:48 +0100

    more stats

--- src/publisher/views/admin/dashboard.jsx
+++ src/publisher/views/admin/dashboard.jsx
@@ -1,22 +1,24 @@
 import React from 'react';
 import clsx from 'clsx';
+import { formatDistanceToNow, subSeconds } from 'date-fns';
+import { enGB, cy } from 'date-fns/locale';
 
 import Layout from '../components/Layout';
 import Table from '../../../shared/views/components/Table';
 
-const Dashboard = (props) => {
-  const { stats, title } = props;
-  const datasetStats = stats.datasets;
-  const columns = [
+const DatasetStats = (props) => {
+  const { summary } = props.stats.datasets;
+
+  const summaryCols = [
     {
       key: 'status',
       format: (value) => {
         if (value === 'total') {
-          return <strong>{props.t('admin.dashboard.stats.datasets.total.label')}</strong>;
+          return <strong>{props.t('admin.dashboard.stats.datasets.summary.total.label')}</strong>;
         }
         return (
           <strong className={clsx('govuk-tag', 'publishing-status', `govuk-tag--${props.statusToColour(value)}`)}>
-            {props.t(`admin.dashboard.stats.datasets.${value}.label`)}
+            {props.t(`admin.dashboard.stats.datasets.summary.${value}.label`)}
           </strong>
         );
       }
@@ -35,13 +37,162 @@ const Dashboard = (props) => {
     }
   ];
 
-  const rows = Object.keys(datasetStats).map((key) => {
+  const summaryRows = Object.keys(summary).map((key) => {
     return {
       status: key,
-      description: props.t(`admin.dashboard.stats.datasets.${key}.description`),
-      count: datasetStats[key]
+      description: props.t(`admin.dashboard.stats.datasets.summary.${key}.description`),
+      count: summary[key]
     };
   });
+
+  const largestCols = [
+    {
+      key: 'title',
+      format: (value, row) => {
+        return (
+          <a className="govuk-link" href={props.buildUrl(`publish/${row.dataset_id}/overview`, props.i18n.language)}>
+            {value}
+          </a>
+        );
+      }
+    },
+    {
+      key: 'row_count',
+      format: (value) => {
+        return value ? Intl.NumberFormat(props.i18n.language).format(value) : '-';
+      }
+    },
+    {
+      key: 'size_bytes',
+      format: (value) => {
+        return value ? `${(value / 1024 / 1024).toFixed(2)} MB` : '-';
+      }
+    }
+  ];
+  const largestRows = props.stats.datasets.largest;
+
+  const longestCols = [
+    {
+      key: 'title',
+      format: (value, row) => {
+        return (
+          <a className="govuk-link" href={props.buildUrl(`publish/${row.dataset_id}/overview`, props.i18n.language)}>
+            {value}
+          </a>
+        );
+      }
+    },
+    {
+      key: 'interval',
+      format: (seconds) => {
+        const locale = props.i18n.language.includes('cy') ? cy : enGB;
+        const start = subSeconds(new Date(), seconds || 0);
+        return seconds ? formatDistanceToNow(start, { locale }) : '-';
+      }
+    },
+    {
+      key: 'status',
+      format: (value) => {
+        return (
+          <span className={clsx('govuk-tag', 'publishing-status', `govuk-tag--${props.statusToColour(value)}`)}>
+            {props.t(`admin.dashboard.stats.datasets.summary.${value}.label`)}
+          </span>
+        );
+      }
+    }
+  ];
+  const longestRows = props.stats.datasets.longest;
+
+  return (
+    <>
+      <div className="govuk-grid-row govuk-!-margin-bottom-5">
+        <div className="govuk-grid-column-full">
+          <h2 className="govuk-heading-m">{props.t('admin.dashboard.stats.datasets.summary.heading')}</h2>
+          <Table i18nBase="admin.dashboard.stats.datasets.summary.table" columns={summaryCols} rows={summaryRows} />
+          <p className="govuk-body-s">{props.t('admin.dashboard.stats.datasets.summary.note')}</p>
+        </div>
+      </div>
+
+      <div className="govuk-grid-row govuk-!-margin-bottom-5">
+        <div className="govuk-grid-column-full">
+          <h2 className="govuk-heading-m">{props.t('admin.dashboard.stats.datasets.largest.heading')}</h2>
+          <Table i18nBase="admin.dashboard.stats.datasets.largest.table" columns={largestCols} rows={largestRows} />
+        </div>
+      </div>
+
+      <div className="govuk-grid-row govuk-!-margin-bottom-5">
+        <div className="govuk-grid-column-full">
+          <h2 className="govuk-heading-m">{props.t('admin.dashboard.stats.datasets.longest.heading')}</h2>
+          <Table i18nBase="admin.dashboard.stats.datasets.longest.table" columns={longestCols} rows={longestRows} />
+        </div>
+      </div>
+    </>
+  );
+};
+
+const UsertStats = (props) => {
+  const userStats = props.stats.users;
+
+  const columns = [
+    {
+      key: 'status',
+      format: (value) => {
+        return value === 'total' ? (
+          <strong>{props.t('admin.dashboard.stats.users.total.label')}</strong>
+        ) : (
+          props.t(`admin.dashboard.stats.users.${value}.label`)
+        );
+      }
+    },
+    {
+      key: 'description',
+      format: (value, row) => {
+        return row.status === 'total' ? <strong>{value}</strong> : value;
+      }
+    },
+    {
+      key: 'count',
+      format: (value, row) => {
+        return row.status === 'total' ? <strong>{value}</strong> : value;
+      }
+    }
+  ];
+
+  const rows = Object.keys(userStats).map((key) => {
+    return {
+      status: key,
+      description: props.t(`admin.dashboard.stats.users.${key}.description`),
+      count: userStats[key]
+    };
+  });
+
+  return (
+    <div className="govuk-grid-row govuk-!-margin-bottom-5">
+      <div className="govuk-grid-column-full">
+        <h2 className="govuk-heading-m">{props.t('admin.dashboard.stats.users.heading')}</h2>
+        <Table i18nBase="admin.dashboard.stats.users.table" columns={columns} rows={rows} />
+      </div>
+    </div>
+  );
+};
+
+const GroupStats = (props) => {
+  const groupStats = props.stats.groups;
+
+  const columns = ['name', 'count'];
+
+  return (
+    <div className="govuk-grid-row govuk-!-margin-bottom-5">
+      <div className="govuk-grid-column-full">
+        <h2 className="govuk-heading-m">{props.t('admin.dashboard.stats.groups.heading')}</h2>
+        <Table i18nBase="admin.dashboard.stats.groups.table" columns={columns} rows={groupStats.most_published} />
+      </div>
+    </div>
+  );
+};
+
+const Dashboard = (props) => {
+  const { title } = props;
 
   return (
     <Layout {...props} formPage title={title}>
@@ -52,12 +203,9 @@ const Dashboard = (props) => {
           </div>
         </div>
 
-        <div className="govuk-grid-row">
-          <div className="govuk-grid-column-full">
-            <h2 className="govuk-heading-l">{props.t('admin.dashboard.stats.datasets.heading')}</h2>
-            <Table i18nBase="admin.dashboard.stats.datasets.table" columns={columns} rows={rows} />
-          </div>
-        </div>
+        <DatasetStats {...props} />
+        <UsertStats {...props} />
+        <GroupStats {...props} />
       </div>
     </Layout>
   );
--- src/shared/interfaces/dashboard-stats.ts
+++ src/shared/interfaces/dashboard-stats.ts
@@ -1,21 +1,30 @@
 export interface DatasetStats {
-  incomplete: number;
-  pending_approval: number;
-  scheduled: number;
-  published: number;
-  action_requested: number;
-  archived: number;
-  offline: number;
-  total: number;
+  summary: {
+    incomplete: number;
+    pending_approval: number;
+    scheduled: number;
+    published: number;
+    action_requested: number;
+    archived: number;
+    offline: number;
+    total: number;
+  };
+  largest: [{ dataset_id: string; title: string; row_count: number; size_bytes?: number }];
+  longest: [{ dataset_id: string; title: string; interval: string; status: string }];
 }
 
 export interface UserStats {
-  total: number;
   active: number;
-  last_7_days: number;
+  published: number;
+  total: number;
+}
+
+export interface UserGroupStats {
+  most_published: [{ name: string; count: number }];
 }
 
 export interface DashboardStats {
   datasets?: DatasetStats;
   users?: UserStats;
+  groups?: UserGroupStats;
 }
