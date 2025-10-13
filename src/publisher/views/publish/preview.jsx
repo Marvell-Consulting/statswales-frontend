bd804ee92a8be729d23746df706cf9cc6040b86a
Phil Moorhouse <phil@wheelsandcogs.co.uk>
2025-10-13 14:38:47 +0100

    fix copilot issue

--- src/publisher/views/publish/preview.jsx
+++ src/publisher/views/publish/preview.jsx
@@ -162,7 +162,15 @@ export default function Preview(props) {
                       >
                         {props.t('buttons.continue')}
                       </button>
-                      <button className="govuk-button govuk-button--secondary" name="confirm" value="false">
+                      <button
+                        type="submit"
+                        name="confirm"
+                        value="false"
+                        className="govuk-button govuk-button--secondary"
+                        data-module="govuk-button"
+                        style={{ verticalAlign: 'unset' }}
+                        data-prevent-double-click="true"
+                      >
                         {props.t('publish.preview.buttons.choose_different')}
                       </button>
                     </div>
