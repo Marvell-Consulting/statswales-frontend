3d9519d6324111366e34a14c395ac2a70073ab69
Phil Moorhouse <phil@wheelsandcogs.co.uk>
2025-10-21 17:31:19 +0100

    area code filter is passed as an object not an array

--- src/shared/utils/parse-filters.ts
+++ src/shared/utils/parse-filters.ts
@@ -1,10 +1,20 @@
+import { isObjectLike } from 'lodash';
 import { FilterInterface } from '../interfaces/filterInterface';
 
 export const parseFilters = (filters?: Record<string, string[]>): FilterInterface[] | undefined => {
   if (!filters) return undefined;
 
-  return Object.keys(filters).map((key: string) => ({
-    columnName: key,
-    values: filters[key].map((value: string) => decodeURIComponent(value))
-  }));
+  return Object.keys(filters).map((columnName: string) => {
+    let values = filters[columnName];
+
+    if (Array.isArray(values)) {
+      values = values.map((value: string) => decodeURIComponent(value));
+    } else if (isObjectLike(values)) {
+      values = Object.values(values).map((value) => decodeURIComponent(value as string));
+    } else {
+      values = [decodeURIComponent(values as string)];
+    }
+
+    return { columnName, values };
+  });
 };
