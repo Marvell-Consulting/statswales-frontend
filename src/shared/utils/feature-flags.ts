c955f6a64c31a598e8ebcb758d0e82667aefcbd3
Phil Moorhouse <phil@wheelsandcogs.co.uk>
2026-02-04 15:12:14 +0000

    support mutliple features

--- src/shared/utils/feature-flags.ts
+++ src/shared/utils/feature-flags.ts
@@ -2,6 +2,7 @@ import { config } from '../config';
 import { AppEnv } from '../config/env.enum';
 import { FeatureFlag } from '../enums/feature-flag';
 
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 type QueryParams = URLSearchParams | Record<string, any>;
 
 export function featureFlaggingDisabled(): boolean {
@@ -9,17 +10,40 @@ export function featureFlaggingDisabled(): boolean {
   return ![AppEnv.Prod, AppEnv.Staging].includes(config.env);
 }
 
-// check if a feature is enabled via query parameters
+// check if a feature is enabled via query parameters, e.g.
+// ?feature=search
+// ?feature=search,analytics
+// ?feature=search&feature=analytics
 export function isFeatureEnabled(params: QueryParams, flag: FeatureFlag): boolean {
   if (featureFlaggingDisabled()) return true;
 
   if (params instanceof URLSearchParams) {
-    return params.get('feature') === flag;
+    const feature = params.get('feature');
+    if (!feature) return false;
+
+    return feature
+      .split(',')
+      .map((f) => f.trim())
+      .includes(flag);
   }
 
   const feature = params.feature;
   if (typeof feature === 'string') {
-    return feature === flag;
+    return feature
+      .split(',')
+      .map((f) => f.trim())
+      .includes(flag);
+  }
+
+  if (Array.isArray(feature)) {
+    return feature.some(
+      (f) =>
+        typeof f === 'string' &&
+        f
+          .split(',')
+          .map((str) => str.trim())
+          .includes(flag)
+    );
   }
 
   return false;
